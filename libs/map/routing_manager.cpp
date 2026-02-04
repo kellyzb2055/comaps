ed167a4161e19d75a7ab6152d6eb3567c0068603
x7z4w <x7z4w@noreply.codeberg.org>
2026-02-04 17:38:43 +0100

    Revert "Revert "[core][drape] Making arrows smooth again""
    Signed-off-by: x7z4w <x7z4w@noreply.codeberg.org>

--- libs/map/routing_manager.cpp
+++ libs/map/routing_manager.cpp
@@ -311,6 +311,7 @@ RoutingManager::RoutingManager(Callbacks && callbacks, Delegate & delegate)
   , m_delegate(delegate)
   , m_extrapolator([this](location::GpsInfo const & gpsInfo) { this->OnExtrapolatedLocationUpdate(gpsInfo); })
 {
+  m_extrapolator.Enable(true);  // Keeps smooth arrow movement whether routing or not
   m_routingSession.Init(
 #ifdef SHOW_ROUTE_DEBUG_MARKS
       [this](m2::PointD const & pt)
@@ -555,10 +556,6 @@ void RoutingManager::RemoveRoute(bool deactivateFollowing)
   if (deactivateFollowing)
   {
     m_transitReadManager->BlockTransitSchemeMode(false /* isBlocked */);
-
-    // Switching on the extrapolator only for following mode in car and bicycle navigation.
-    m_extrapolator.Enable(m_currentRouterType == RouterType::Vehicle || m_currentRouterType == RouterType::Bicycle);
-
     // Remove all subroutes.
     m_drapeEngine.SafeCall(&df::DrapeEngine::RemoveSubroute, dp::DrapeID(), true /* deactivateFollowing */);
   }
@@ -769,7 +766,6 @@ void RoutingManager::FollowRoute()
 
 void RoutingManager::CloseRouting(bool removeRoutePoints)
 {
-  m_extrapolator.Enable(false);
   // Hide preview.
   HidePreviewSegments();
 
@@ -946,9 +942,9 @@ void RoutingManager::SetPointsFollowingMode(bool enabled)
 
 void RoutingManager::RemovePassedPoints()
 {
-  ASSERT(m_bmManager != nullptr, ());
-  RoutePointsLayout routePoints(*m_bmManager);
-  routePoints.RemovePassedPoints();
+    ASSERT(m_bmManager != nullptr, ());
+    RoutePointsLayout routePoints(*m_bmManager);
+    routePoints.RemovePassedPoints();
 }
 
 void RoutingManager::ReorderIntermediatePoints()
