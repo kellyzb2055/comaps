kellyzb2055/comaps

commit 39c0d9404ca68f619d5eaead6eb828c085d3f0a3

[generator] Add support of location of defibrillator
Signed-off-by: Jean-Baptiste <jeanbaptiste.charron@outlook.fr>

generator/osm2meta.cpp
@@ -559,7 +559,7 @@ void MetadataTagProcessor::operator()(std::string const & k, std::string const &
     return std::string();
   };
 
-  if (k.starts_with("description"))
+  if (k.starts_with("description") || k.starts_with("defibrillator:location")
   {
     // Separate description tags processing.
     localisation::LanguageIndex languageIndex = localisation::kDefaultNameIndex;

commit 7fca27bc0f4c16c4a24911cd6ddb21b15f9a2c08

generator: osm2meta: fix syntax
fix syntax error introduced in
39c0d9404ca68f619d5eaead6eb828c085d3f0a3

Signed-off-by: Wojciech Sipak <wsipak@protonmail.com>

generator/osm2meta.cpp
@@ -559,7 +559,7 @@ void MetadataTagProcessor::operator()(std::string const & k, std::string const &
     return std::string();
   };
 
-  if (k.starts_with("description") || k.starts_with("defibrillator:location")
+  if (k.starts_with("description") || k.starts_with("defibrillator:location"))
   {
     // Separate description tags processing.
     localisation::LanguageIndex languageIndex = localisation::kDefaultNameIndex;