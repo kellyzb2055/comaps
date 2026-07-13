kellyzb2055/comaps

commit e760f7c35dece0cf6eb25d08b2aa182d1b2ad506

[core] Skip loading info of features with deprecated or unsupported types
Signed-off-by: Konstantin Pastbin <konstantin.pastbin@gmail.com>

libs/map/framework.cpp
@@ -722,7 +722,13 @@ void Framework::FillPointInfo(place_page::Info & info, m2::PointD const & mercat
   auto const fid = GetFeatureAtPoint(mercator, std::move(matcher));
   if (fid.IsValid())
   {
-    m_featuresFetcher.GetDataSource().ReadFeature([&](FeatureType & ft) { FillInfoFromFeatureType(ft, info); }, fid);
+    m_featuresFetcher.GetDataSource().ReadFeature([&](FeatureType & ft) {
+      FillInfoFromFeatureType(ft, info);
+      // If all types are either deprecated or unsupported (new maps in older app),
+      // then act like its just a map point without features.
+      if (info.GetTypes().Empty())
+        FillNotMatchedPlaceInfo(info, mercator, customTitle);
+    }, fid);
     // This line overwrites mercator center from area feature which can be far away.
     info.SetMercator(mercator);
   }
@@ -751,14 +757,17 @@ void Framework::FillPostcodeInfo(string const & postcode, m2::PointD const & mer
 
 void Framework::FillInfoFromFeatureType(FeatureType & ft, place_page::Info & info) const
 {
+  info.SetFromFeatureType(ft);
+  auto const & types = info.GetTypes();
+  if (types.Empty())
+    return;
+
   auto const featureStatus = osm::Editor::Instance().GetFeatureStatus(ft.GetID());
   ASSERT_NOT_EQUAL(featureStatus, FeatureStatus::Deleted, ("Deleted features cannot be selected from UI."));
   info.SetFeatureStatus(featureStatus);
 
-  if (ftypes::IsAddressObjectChecker::Instance()(ft))
+  if (ftypes::IsAddressObjectChecker::Instance()(types))
     info.SetAddress(GetAddressAtPoint(feature::GetCenter(ft)).FormatAddress());
-
-  info.SetFromFeatureType(ft);
 
   FillReviews(ft, info);
   FillDescriptions(ft, info);
@@ -769,7 +778,6 @@ void Framework::FillInfoFromFeatureType(FeatureType & ft, place_page::Info & inf
   info.SetCanEditOrAdd(canEditOrAdd);
 
   // Fill countryId for place page info
-  auto const & types = info.GetTypes();
   bool const isState = ftypes::IsStateChecker::Instance()(types);
   if (isState || ftypes::IsCountryChecker::Instance()(types))
   {

libs/indexer/map_object.cpp
@@ -23,17 +23,15 @@ using namespace std;
 
 void MapObject::SetFromFeatureType(FeatureType & ft)
 {
-  m_mercator = feature::GetCenter(ft);
-  m_name = ft.GetNames();
-
   Classificator const & cl = classif();
   m_types = feature::TypesHolder(ft);
   m_types.RemoveIf([&cl](uint32_t t) { return !cl.IsTypeValid(t); });
-  // Actually, we can't select object on map with invalid (non-drawable or deprecated) type.
-  // TODO: in android prod a user will see an "empty" PP if a spot is selected in old mwm
-  // where a deprecated feature was; and could crash if play with routing to it, bookmarking it..
-  // A desktop/qt prod segfaults when trying to select such spots.
-  ASSERT(!m_types.Empty(), ());
+  // All types might be either deprecated or unsupported (new maps in old app).
+  if (m_types.Empty())
+    return;
+
+  m_mercator = feature::GetCenter(ft);
+  m_name = ft.GetNames();
 
   m_metadata = ft.GetMetadata();
   m_houseNumber = ft.GetHouseNumber();
@@ -108,7 +106,9 @@ std::string_view MapObject::GetPostcode() const
 
 std::string MapObject::GetLocalizedType() const
 {
-  ASSERT(!m_types.Empty(), ());
+  if (m_types.Empty())
+    return {};
+
   feature::TypesHolder copy(m_types);
   copy.SortBySpec();
 
@@ -117,7 +117,9 @@ std::string MapObject::GetLocalizedType() const
 
 std::string MapObject::GetLocalizedAllTypes(bool withMainType) const
 {
-  ASSERT(!m_types.Empty(), ());
+  if (m_types.Empty())
+    return {};
+
   feature::TypesHolder copy(m_types);
   copy.SortBySpec();
 
@@ -143,7 +145,7 @@ std::string MapObject::GetLocalizedAllTypes(bool withMainType) const
         continue;
       }
     }
-    
+
     // Ignore types that never should be main types
     if ((isMainType || isOnlyTypeTouristAttraction) && isNeverMainTypeChecker(type))
       continue;
@@ -288,7 +290,7 @@ std::string MapObject::GetOrganic() const
     if (isOrganic(type))
       return localisation::TranslatedFeatureType(classif().GetReadableObjectName(type));
   }
-  
+
   return {};
 }
 

commit 873cc45114189b81585773be1743f666401bc1bc

[core] Set feature address before forming title/subtitle strings
Fixes a regression in e760f7c35 [core] Skip loading info of features with deprecated or unsupported types

Signed-off-by: Konstantin Pastbin <konstantin.pastbin@gmail.com>

libs/map/framework.cpp
@@ -759,17 +759,20 @@ void Framework::FillPostcodeInfo(string const & postcode, m2::PointD const & mer
 
 void Framework::FillInfoFromFeatureType(FeatureType & ft, place_page::Info & info) const
 {
-  info.SetFromFeatureType(ft);
-  auto const & types = info.GetTypes();
-  if (types.Empty())
-    return;
 
   auto const featureStatus = osm::Editor::Instance().GetFeatureStatus(ft.GetID());
   ASSERT_NOT_EQUAL(featureStatus, FeatureStatus::Deleted, ("Deleted features cannot be selected from UI."));
   info.SetFeatureStatus(featureStatus);
 
-  if (ftypes::IsAddressObjectChecker::Instance()(types))
+  if (ftypes::IsAddressObjectChecker::Instance()(ft))
     info.SetAddress(GetAddressAtPoint(feature::GetCenter(ft)).FormatAddress());
+
+  // SetFromFeatureType() expects an address to be set already
+  // TODO: move address setting inside of it?
+  info.SetFromFeatureType(ft);
+  auto const & types = info.GetTypes();
+  if (types.Empty())
+    return;
 
   FillReviews(ft, info);
   FillDescriptions(ft, info);